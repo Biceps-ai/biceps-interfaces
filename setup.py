2655a160ec8b708ff54d0e3cbab3018634941e20
NPBiceps <niels.pchn@gmail.com>
2023-12-19 15:04:53 +0100

    fix: fix packaging

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import setup
 
 
-with open("requirements.txt", "r") as fp:
-    requirements = [line.strip() for line in fp.readlines()]
-
-
 setup(
-    name='biceps_interfaces',
+    name='biceps-interfaces',
     version='0.0.1',
     authors = [
         {'name': "Niels Pichon", 'email': "niels@biceps.ai"},
     ],
-    install_requires=requirements,
+    install_requires=[
+        "dataclasses-json>=0.6.3",
+    ],
 )
