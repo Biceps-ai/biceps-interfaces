5209c7a064498fb381a21f6e5546226acd360059
NPBiceps <niels.pchn@gmail.com>
2023-12-29 09:51:24 +0100

    chore: increased the version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='biceps-interfaces',
-    version='0.0.2',
+    version='0.1.0',
     authors = [
         {'name': "Niels Pichon", 'email': "niels@biceps.ai"},
     ],
